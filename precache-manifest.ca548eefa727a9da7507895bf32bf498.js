1f383f461534c66b6ba8dcf0bb3bb15cd84b0a80
Shatya <KingShatya1@gmail.com>
2020-11-08 15:05:39 +0700

    Updates

---
+++ precache-manifest.ca548eefa727a9da7507895bf32bf498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f76b326bbfda738b5a98a9caf2cf0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fd93b8b5ec2fd640316",
+    "url": "/static/css/main.d60160a2.chunk.css"
+  },
+  {
+    "revision": "8b307ea7bd644cdd3617",
+    "url": "/static/js/2.57acd71f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.57acd71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd93b8b5ec2fd640316",
+    "url": "/static/js/main.cdf73f87.chunk.js"
+  },
+  {
+    "revision": "38afd928c3938bf11b4b",
+    "url": "/static/js/runtime-main.dad804b2.js"
+  }
+]);
